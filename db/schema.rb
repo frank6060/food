29b3c8fbcd8435d04c765473f8bf48aa3132f1c9
frank <fbyiadom@gmail.com>
2017-04-21 02:37:06 +0000

    added seed data to application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421013550) do
+ActiveRecord::Schema.define(version: 20170421023348) do
 
   create_table "org_companies", force: :cascade do |t|
     t.string   "name",             limit: 255,   null: false
